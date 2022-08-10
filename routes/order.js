fb6d1077e5dbd073feca14287b121c4bd53b92b7
sammie <51814698+ogoigbe12@users.noreply.github.com>
2022-08-10 20:22:48 +0100

    update made to gitignore

---
+++ routes/order.js
@@ -0,0 +1,98 @@
+const Order = require("../models/Order")
+const {
+    veriftyToken,
+    veriftyTokenAndAuthorization,
+    veriftyTokenAndAdmin,
+} = require("./verifyToken")
+
+const router = require("express").Router()
+
+//CREATE    
+
+router.post("/", async (req,res)=>{
+     const newOrder = new Order(req.body)
+
+     try{
+        const savedOrder = await newOrder.save()
+        res.status(200).json(savedOrder)
+     }catch(err){
+        res.status(500).json(err)
+     }
+})
+
+//UPDATE 
+router.put("/:id",  async (req,res)=>{
+    try{
+        const updatedOrder = await savedOrder.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        },{new:true})
+        res.status(200).json(updatedOrder)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//DELETE
+
+router.delete("/:id",  async(req,res)=>{
+    try {
+        await Order.findByIdAndDelete(req.params.id)
+        res.status(200).json("Cart has been deleted....")
+    }catch(err){ 
+        res.status(500).json(err)
+    }
+})
+
+//GET USER CART ORDERS
+router.get("/find/:userId", async(req,res)=>{
+    try {
+        const orders = await order.find({userId: req.params.userId})
+        
+        res.status(200).json(orders)
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+// //GET ALL 
+
+router.get("/", async (req, res)=> {
+    try {
+        const orders = await Order.find()
+        res.status(200).json(orders)
+    }catch (err){
+        res.status(500).json(err)
+    }
+})
+
+//GET MONTHLY INCOME
+
+router.get("/income", async (req, res) => {
+    const date = new Date()
+    const lastMonth = new Date(date.setMonth(date.getMonth() -1))
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() -1))
+
+    try{
+        const income = await Order.aggregate([
+            {$match: {createdAt: {$gte: previousMonth } } },
+            {
+                $project: {
+                    month: {$month: "$createdAt"},
+                    sales: "$amount",
+                },
+            },
+            {
+              $group: {
+                _id: "$month",
+                total: { $sum: "$sales"},
+              },
+            },
+            ])
+            res.status(200).json(income)
+    }catch (err){
+        res.status(500).json(err)
+    }
+})
+
+
+module.exports = router
